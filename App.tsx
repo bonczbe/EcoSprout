f488a07a38d25c70bbc93baff9956f155d4f32c0
Bence Boncz <bonczbe@gmail.com>
2024-11-02 14:36:04 +0100

    removed nativewind, added twrnc

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import "./global.css";
+import tw from "twrnc";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw`bg-red-500`}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
